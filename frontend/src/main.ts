540bb7cc86585e77b1c09a41379d88839a57ace7
Veronica <veronica.bergstrom@medieinstitutet.se>
2023-04-05 10:48:00 +0200

    tinymce installed in frontend and tested

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,1 +1,24 @@
+import tinymce from "tinymce/tinymce";
 
+tinymce.init({
+  selector: "#textContent",
+  plugins: "code",
+  toolbar: "code",
+
+  setup: function (editor) {
+    editor.on("change", function () {
+      editor.save();
+    });
+  },
+});
+
+document.getElementById("saveBtn")!.addEventListener("click", () => {
+  let textContent = (document.getElementById("textContent") as HTMLInputElement)
+    .value;
+  let textResult = document.querySelector(".textResult");
+  console.log("textContent:", textContent);
+  console.log("textResult:", textResult);
+  if (textResult) {
+    textResult.innerHTML = textContent;
+  }
+});
