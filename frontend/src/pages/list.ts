a12c9d0e247f17400d8d801f35ef1f3b6616e737
Veronica <veronica.bergstrom@medieinstitutet.se>
2023-04-14 14:19:39 +0200

    added a type Note object

--- frontend/src/pages/list.ts
+++ frontend/src/pages/list.ts
@@ -3,24 +3,56 @@ import { preview } from "./preview";
 
 const app = document.getElementById("app") as HTMLDivElement;
 
+type Note = {
+  id: number;
+  title: string;
+  author: {
+    name: string;
+  };
+};
+
+// render list page and functionality
 export async function list() {
+  const header = document.createElement("h1");
+  const subhHeader = document.createElement("h3");
+
+  header.innerText = "Välkommen till Notes4You";
+  subhHeader.innerText = "Skapade dokument:";
+
+  app.append(header, subhHeader);
+
   const result = await fetch("http://localhost:3000/api/notes");
-  const notes = await result.json();
-  console.log(notes);
+  const notes = (await result.json()) as Note[];
   const ul = document.createElement("ul");
-  notes.forEach((note: { id: any; title: string }) => {
+  notes.forEach((note) => {
     const li = document.createElement("li");
     li.innerHTML = `
+      <div class="listItems">
           <span>${note.title}</span>
+          <span>${note.author.name}</span>
+          </div>
+          <div class="listButtons">
             <button data-id="${note.id}" class="showBtn">visa</button>
             <button data-id="${note.id}" class="editBtn">redigera</button>
+          
+      </div>
           `;
     ul.appendChild(li);
   });
   app.append(ul);
 
-  //FETCH AND SHOW TITLE AND CONTENT BY CLICKING SHOWBTN
+  // newNote Button to create a new note
+  const newNoteBtn = document.createElement("button");
+  newNoteBtn.innerText = "Skapa nytt";
+  newNoteBtn.classList.add("newNoteBtn");
 
+  app.appendChild(newNoteBtn);
+
+  newNoteBtn.addEventListener("click", () => {
+    edit();
+  });
+
+  //show button, show note in uneditable state
   document.querySelectorAll<HTMLButtonElement>(".showBtn").forEach((button) => {
     button.onclick = async (e) => {
       if (!(e.currentTarget instanceof HTMLButtonElement)) {
@@ -31,6 +63,7 @@ export async function list() {
     };
   });
 
+  //edit button, open tinymce to edit excisting note
   document.querySelectorAll<HTMLButtonElement>(".editBtn").forEach((button) => {
     button.onclick = async (e) => {
       if (!(e.currentTarget instanceof HTMLButtonElement)) {
@@ -39,5 +72,16 @@ export async function list() {
       const id = e.currentTarget?.dataset.id;
       edit(id);
     };
+  });
+
+  //log out button
+  const logoutBtn = document.createElement("button");
+  logoutBtn.innerText = "Logga ut";
+  logoutBtn.classList.add("logoutBtn");
+  app.appendChild(logoutBtn);
+
+  logoutBtn.addEventListener("click", async () => {
+    localStorage.removeItem("user");
+    location.reload();
   });
 }
